8cd186dc5068b7687e76d0c3ded127b58530ae4b
nqm314 <minhnq220804@gmail.com>
2025-10-05 09:06:14 +0700

    Update information box

--- frontend/src/components/StepPlay.tsx
+++ frontend/src/components/StepPlay.tsx
@@ -60,6 +60,27 @@ export default function StepPlay() {
         { id: 9, name: "NPK (Stage 3 – Lam Rong)", key: "NPK in stage 3 (NPK_lam_rong)" },
     ];
 
+    const weather_data = {
+        "Winter–Spring": {
+            '1': {"temp": 26.58, "rain": 169.37, "humidity": 88.63},
+            '2': {"temp": 26.42, "rain": 35.78, "humidity": 85.47},
+            '3': {"temp": 26.81, "rain": 3.12, "humidity": 79.17},
+            '4': {"temp": 28.29, "rain": 0.0, "humidity": 67.58}
+        },
+        "Summer–Autumn": {
+            '1': {"temp": 33.44, "rain": 51.37, "humidity": 54.85},
+            '2': {"temp": 29.75, "rain": 188.81, "humidity": 79.25},
+            '3': {"temp": 28.05, "rain": 218.75, "humidity": 85.24},
+            '4': {"temp": 27.22, "rain": 402.63, "humidity": 90.10}
+        },
+        "Autumn–Winter": {
+            '1': {"temp": 27.19, "rain": 253.74, "humidity": 88.98},
+            '2': {"temp": 27.05, "rain": 294.69, "humidity": 88.80},
+            '3': {"temp": 26.91, "rain": 59.77, "humidity": 85.97},
+            '4': {"temp": 25.18, "rain": 53.18, "humidity": 85.05}
+        }
+    }
+
     const [stageInfoOpen, setStageInfoOpen] = useState(false);
     const [stageInfo, setStageInfo] = useState<any>(null);
     const [loadingInfo, setLoadingInfo] = useState(false);
@@ -237,9 +258,9 @@ export default function StepPlay() {
                     onClick={() => setChartOpen(true)}
                 >
                     <Typography variant="h6">📈</Typography>
-                    <Typography variant="h6">temp: 123</Typography>
-                    <Typography variant="h6">moist: 123</Typography>
-                    <Typography variant="h6">light: 123</Typography>
+                    <Typography variant="h6">Average Temperature: {weather_data[season.name][stage + 1].temp}°C</Typography>
+                    <Typography variant="h6">Total Rainfall: {weather_data[season.name][stage + 1].rain}mm</Typography>
+                    <Typography variant="h6">Average Humidity: {weather_data[season.name][stage + 1].humidity}%</Typography>
                 </Box>
             </Box>
 
